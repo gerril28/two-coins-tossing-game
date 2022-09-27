a2022282ed23192fd881214f0f0937676c0deb51
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-27 09:49:12 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 input.onButtonPressed(Button.A, function () {
-    let Wins = 0
     if (Player_1 == Wins) {
     	
     }
 })
+let Wins = 0
 let Player_1 = 0
 Player_1 = 0
 let Player_2 = 0
+Wins = 0
