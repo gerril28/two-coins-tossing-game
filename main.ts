67df4c19ee0e1aa128b73e3b76765450fb4dbd00
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-28 09:39:47 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,8 +7,11 @@ input.onButtonPressed(Button.A, function () {
     } else if (Player_1 == (Tails && Tails)) {
         basic.pause(2000)
         basic.showString("P1")
-        basic.showString("L")
-        Points += -1
+        basic.showString("W")
+        Points += 1
+    } else if (Player_2 == (Tails && Tails)) {
+        basic.pause(2000)
+        basic.showString("P1")
     } else {
     	
     }
@@ -16,8 +19,11 @@ input.onButtonPressed(Button.A, function () {
 let Points = 0
 let Tails = 0
 let Heads = 0
+let Player_2 = 0
 let Player_1 = 0
+basic.showNumber(Player_1)
+basic.showNumber(Player_2)
 Player_1 = 1
-let Player_2 = 2
+Player_2 = 2
 Heads = 0
 Tails = 0
