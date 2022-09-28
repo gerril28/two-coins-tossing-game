ddae0b37d82ccc484d1f3298792561eb5c8f8bd7
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-28 09:23:35 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
 input.onButtonPressed(Button.A, function () {
-    let Heads = 0
-    if (Player_1 == (Heads && Heads)) {
-        basic.showString("W")
-    } else {
+    if (Player_1 == (Heads == Heads)) {
     	
     }
 })
+let Heads = 0
 let Player_1 = 0
-Player_1 = 0
-let Player_2 = 0
-let Wins = 0
+Player_1 = 1
+let Player_2 = 2
+Heads = 0
+let Tails = 0
